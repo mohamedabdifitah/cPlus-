45d7f09e7d63a511e020bf0b2533e2e28dcfa6bf
mohamedabdifitah <learntecnology30@gmail.com>
2022-08-05 17:28:31 +0300

    initial

---
+++ hello.cpp
@@ -0,0 +1,30 @@
+// C++ program to display "Hello World" 
+// Header file for input output functions 
+#include <iostream> 
+#include <string>
+using namespace std; 
+
+// function declaration 
+void greet() {
+  cout << "Hello World"<<endl; 
+  
+}
+int main() 
+
+{ 
+  // prints hello world 
+  //std::cout << "hello world" << std::endl;
+  cout << "second hello world"<<endl;
+  string hello = "sweetcoin ";
+  float x ;
+  cout << "Enter a number"<<endl;
+  cin >> x;
+  cout << x*2<<endl;
+  
+  cout << hello<<endl;
+  
+  greet();
+  return 0; 
+
+}
+
